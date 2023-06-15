a92b475e25f7aa4678eb349511c8e62d236336ff
hatmath <mathieuhatin@hotmail.com>
2023-06-15 14:37:41 -0400

    infoAPI était en commentaire dsl

--- backend/server/express.js
+++ backend/server/express.js
@@ -32,7 +32,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
     console.log("Connected successfully, to Fetch API data");
-    // apiInfo();
+    apiInfo();
     });
 
 app.use(cors());
