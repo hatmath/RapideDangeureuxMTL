ccf0df48b35bb5c94c04702e86f3211fa7c6e674
hatmath <mathieuhatin@hotmail.com>
2023-06-16 12:17:24 -0400

    fautes d'orthographe

--- frontend/webpage.js
+++ frontend/webpage.js
@@ -10,15 +10,15 @@ window.onload = async function() {
 
     // Populate data store to textboxes
     document.getElementById('statname_01').innerText = "Date de l’accident";
-    document.getElementById('statname_02').innerText = "Nombre de vehicules impliques";
+    document.getElementById('statname_02').innerText = "Nombre de véhicules impliqués";
     document.getElementById('statname_03').innerText = "Localisation longitudinale";
     document.getElementById('statname_04').innerText = "Code du point cardinal";
     document.getElementById('statname_05').innerText = "Aspect de la route";
-    document.getElementById('statname_06').innerText = "Accident en 2017 impliquant un arbre";
-    document.getElementById('statname_07').innerText = "Accident en 2019 dans une zone de 40km avec la condition meteo (11-Clair)";
-    document.getElementById('statname_08').innerText = "Accident en 2016 impliquant un véhicule d'urgence sur une route categorie (14-rue résidentielle)";
-    document.getElementById('statname_09').innerText = "Accident en 2014 le lundi";
-    document.getElementById('statname_10').innerText = "Accident en 2013 le vendredi";
+    document.getElementById('statname_06').innerText = "Accidents en 2017 impliquant un arbre";
+    document.getElementById('statname_07').innerText = "Accidents en 2019 dans une zone de 40km avec la condition meteo (11-Clair)";
+    document.getElementById('statname_08').innerText = "Accidents en 2016 impliquant un véhicule d'urgence sur une route categorie (14-rue résidentielle)";
+    document.getElementById('statname_09').innerText = "Accidents en 2014 le lundi";
+    document.getElementById('statname_10').innerText = "Accidents en 2013 le vendredi";
 
     document.getElementById('statdata_01').innerText = data.date;
     document.getElementById('statdata_02').innerText = data.borne;
